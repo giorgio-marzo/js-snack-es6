42f9255e301d794b474769101c140ab696cadf17
giorgio-marzo <gio.marzo96@gmail.com>
2025-03-28 15:42:55 +0100

    [UPD] snack 1 completato

--- js/script.js
+++ js/script.js
@@ -13,6 +13,19 @@ const bici = [
 
 ]
 
+// definisco una variabile di partenza della prima posizione dell'elemento dell'array da confronare con le sucessive
+let biciLeggera = bici[0]
+
+//ciclo l'array
+for(let i=0; i<bici.length; i++){
+
+    if(biciLeggera.peso > bici[i].peso){
+        biciLeggera = bici[i]
+    }
+
+}
+
+console.log(biciLeggera)
 
 /*
 Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
