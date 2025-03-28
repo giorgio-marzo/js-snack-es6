e478a01f543dab7e85adc4ddd9363143b13c3730
giorgio-marzo <gio.marzo96@gmail.com>
2025-03-28 15:48:19 +0100

    [UPD] creazione array di oggetti (squadre)

--- js/script.js
+++ js/script.js
@@ -31,3 +31,13 @@ console.log(biciLeggera)
 Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
+// creazione array di squadre 
+const team = [
+    {name: 'Juve', punti:0, falli:0},
+    {name: 'Milan', punti:0, falli:0},
+    {name: 'Inter', punti:0, falli:0},
+    {name: 'Roma', punti:0, falli:0},
+
+]
+
+
